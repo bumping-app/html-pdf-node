681592cb8339af5f75bdcb32df060e4334222e4b
Chris Cheung <Chris@bumping.app>
2022-11-15 20:31:26 +0800

    Getting export to work

--- index.js
+++ index.js
@@ -59,15 +59,89 @@ async function generatePdfs(files, options, callback) {
     args: args
   });
   let pdfs = [];
+  let count = 0;
   const page = await browser.newPage();
+  await page.emulateMediaType('print');
+  //background-repeat: repeat-y;
+  //background-attachment: fixed;
+  //background-size: 100% auto;
+  // border: 2px solid black;
+  //position: fixed; /* Sit on top of the page content */
+  // #bodydiv
+  // @page {
+  //   size: A4 portait;
+  //   margin: 0;
+  // }
+  // 420 x 595 pixels
+  // size: A4 portait;
+  // 140px
+  // , linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)) 
+  // background-image: url('https://bumping-files.s3.ap-east-1.amazonaws.com/uploads/59tytdtbrasdo2slf2m80-Bump-bump-imageimage.JPEG'), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5));
+            // background-size: 100% 100% ;
+            // background-blend-mode: overlay;
+            // background-repeat: repeat;
+            // background-position: left bottom;
+          //}
   for(let file of files) {
     if(file.content) {
-      data = await inlineCss(file.content, {url:"/"})
-      console.log("Compiling the template with handlebars")
+      data = await inlineCss(file.content, {
+        url:"/", 
+        extraCss:`
+          @page {
+            margin: 0;
+          }
+          html {
+            -webkit-print-color-adjust: exact;
+            margin: 0;
+          }
+          body {
+            margin: 0;
+            color: white;
+          //   position: absolute;
+          //   z-index: 9999;
+          //   top: 0;
+          //   background-image: url('https://bumping-files.s3.ap-east-1.amazonaws.com/uploads/59tytdtbrasdo2slf2m80-Bump-bump-imageimage.JPEG'), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5));
+          //   background-size:  0 0/100% 100vh;
+          //   background-repeat: repeat;
+          //   background-position: fixed; 
+          }
+          #bodydiv {
+            margin: 0;
+          }
+          #contentwrapper {
+            padding-left: 50px;
+            padding-right: 50px;
+            padding-top: 0px;
+            padding-bottom: 0px;
+          }
+          img {
+            border-radius: 15px;
+            width: 100%;
+            height: auto;
+          }
+          .pline {
+            display: inline;
+          }
+          .headerbox {
+            background-color: rgba(0,0,0,.5);
+            color: #fff;
+            border-radius: 15px;
+            margin-left: 25px;
+            padding-left: 25px;
+            margin-right: 25px;
+            padding-right: 25px;
+            padding-bottom: 10px;
+            margin-top: 20px;
+            padding-top: 10px;
+          }
+        `
+      })
+      
       // we have compile our code with handlebars
       const template = hb.compile(data, { strict: true });
       const result = template(data);
       const html = result;
+      console.log("Compiling the template with handlebars", count++, html);
       // We set the page content as the generated html by handlebars
       await page.setContent(html, {
         waitUntil: 'networkidle0', // wait for page to load completely
